71c4b988e7ead00a2f3f7c9a6c7766393bbf8543
dylanlinux <accounts@dylankirdahy.nyc>
2022-06-27 13:50:41 -0400

    Basic functionality working. Buttons 1-8 each turn on a channel. Button 9 turns on all channels. Button 10 turns off all channels.

--- portias-lighting-control/portias-lighting-control.ino
+++ portias-lighting-control/portias-lighting-control.ino
@@ -4,6 +4,21 @@ namespace teensydmx = ::qindesign::teensydmx;
 
 teensydmx::Sender dmxTx{Serial1};
 
+// Channels:
+// 1 - Burnt out or not connected
+// 2 - Backdrop lighting
+// 3 - Burnt out or not connected
+// 4 - Spotlight pointing at stage-left
+// 5 - Another spotlight point at stage-left (but from further away)
+// 6 - A spotlight pointing at the pillar all the way stage-left
+// 7 - A spotlight pointing at the center of the stage from the very back of the room stage-right
+// 8 - A spotlight that shines at the center of the audience seating from stage-right. Also another spotlight pointing center stage
+// 9 - A spotlight that points at stage-right from stage-right
+// 10 - Burnt out or not connected
+// 11 - A spotlight pointing at the piller all the way stage-right
+// 12 - Burnt out or not connected
+
+
 void setup() {
 
 	for(int i = 14; i < 24; i++) {
@@ -14,10 +29,50 @@ void setup() {
 }
 
 void loop() {
+	//light_test(2);
+	if(digitalRead(23) == LOW) { //Button 1
+		dmxTx.set(2, 255);
+	}
+	if(digitalRead(22) == LOW) { //Button 2
+		dmxTx.set(4, 255);
+	}
+	if(digitalRead(21) == LOW) { //Button 3
+		dmxTx.set(5, 255);
+	}
+	if(digitalRead(20) == LOW) { //Button 4
+		dmxTx.set(6, 255);
+	}
+	if(digitalRead(19) == LOW) { //Button 5
+		dmxTx.set(7, 255);
+	}
+	if(digitalRead(18) == LOW) { //Button 6
+		dmxTx.set(8, 255);
+	}
+	if(digitalRead(17) == LOW) { //Button 7
+		dmxTx.set(9, 255);
+	}
+	if(digitalRead(16) == LOW) { //Button 8
+		dmxTx.set(11, 255);
+	}
+	if(digitalRead(15) == LOW) { //Button 9
+		for(int i = 1; i < 13; i++) {
+			dmxTx.set(i, 255);
+		}
+	}
+	if(digitalRead(14) == LOW) { //Off Button
+		for(int i = 1; i < 13; i++) {
+			dmxTx.set(i, 0);
+		}
+	}
+}
+
+
+
+void light_test(int channel) {
 	digitalWrite(13, HIGH);
-	dmxTx.set(2, 255);
+	dmxTx.set(channel, 255);
 	delay(1000);
 	digitalWrite(13, LOW);
-	dmxTx.set(2, 0);
+	dmxTx.set(channel, 0);
 	delay(1000);
 }
